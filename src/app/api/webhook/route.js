4ed4804f5c04bf01bc4dc2db020f3f933de42f21
Jakubcho <jakub.chola92@gmail.com>
2024-02-16 13:58:05 +0100

    webhooks

--- src/app/api/webhook/route.js
+++ src/app/api/webhook/route.js
@@ -6,13 +6,14 @@ export default async function POST(req) {
   const sig = req.headers.get('stripe-signature');
   let event;
   try {
-    event = stripe.webhooks.constructEvent(await buffer(req), sig, signSecret);
+    const reqBuffer = await req.text();
+    const signSecret = process.env.STRIPE_SIGN_SECRET;
+    event = stripe.webhooks.constructEvent(reqBuffer, sig, signSecret);
   } catch(e){
     console.err('stripe error');
     console.log(e);
     return Response.json(e, {status:400});
   }
-  const metadata = event?.data?.object?.orderId;
 
   if(event.type === 'checkout.session.completed'){
     const orederId = event?.data?.object?.metadata?.orderId;
